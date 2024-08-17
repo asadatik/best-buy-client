4f2bc9cc6a1fbe275492f0b5ea7de3e7ab507306
Asad Atik <asadatik1995@gmail.com>
2024-08-17 18:48:48 +0600

    Pagination added

---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import { FcHome } from "react-icons/fc";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io";
+
+import { MdEmail } from "react-icons/md";
+const Footer = () => {
+    return (
+        <>
+       <footer className="footer  bg-indigo-600   mt-14 p-4  lg:p-10  text-base-content">
+  <nav>
+     <div className="flex gap-1 " >
+     <FcHome className="text-3xl" />
+     <h6 className="footer-title text-2xl font-bold  text-orange-400 ">ShopSort</h6> 
+        
+     </div>
+    <a className="link  text-white link-hover">Collins Street West, Victoria 8007, Australia.</a>
+    <a className="link   text-white link-hover">+1 246-345-0695</a>
+    <a className="link   text-white link-hover flex gap-1 "><MdEmail  className="text-lg" />   shosort09@gmail.com</a>
+   
+  </nav> 
+  <nav className="text-white"   >
+    <h6 className="footer-title  text-xl ">navigation</h6> 
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Blogs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  {/* <nav   className="text-white"  >
+    <h6 className="footer-title  text-xl   ">The Highlights</h6> 
+    <a className="link link-hover">Pc</a>
+    <a className="link link-hover">Laptop</a>
+    <a className="link link-hover">Monitor</a>
+    <a className="link link-hover">Camera</a>
+  </nav> */}
+</footer> 
+<footer className="footer  px-10 py-4 border  bg-indigo-500 text-base-content border-base-300">
+  <aside className="items-center grid-flow-col">
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+    <p>ShopSort  Ltd. <br/>Providing reliable tech since 1992</p> 
+  </aside> 
+  <nav className="md:place-self-center  md:justify-self-end">
+    <div className="grid grid-flow-col gap-4">
+          
+    <FaFacebookSquare  className=" text-3xl text-white "  />
+    <FaSquareXTwitter className=" text-3xl text-white"   />
+    <IoLogoYoutube   className=" text-3xl  text-white "  />
+     </div>
+  </nav>
+</footer>
+    </>
+    );
+};
+
+export default Footer;
